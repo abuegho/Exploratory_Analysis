6e4988657ad4c6440f7b77da02f3408fd8106228
abuegho <eghof@unlv.nevada.edu>
2018-01-25 22:50:32 -0800

    2s

--- Script.R
+++ Script.R
@@ -1,5 +1,12 @@
-W = unite(Traffic_Violations, DateTime, Date, Time, sep = " ")
+## Combine date and time into one column and arrange df by this new variable
+W = Traffic_W %>% 
+  unite(DateTime, Date, Time, sep = " ") %>% 
+  arrange(as.POSIXct(DateTime, format = "%m/%d/%Y %H:%M:%S"))
 
-W = arrange(W, as.POSIXct(DateTime, format = "%m/%d/%Y %H:%M:%S"))
+## Create a second data frame containing unique events (since original data set
+## can have multiple observations referring to the same person)
 
 Single = distinct(W, DateTime, .keep_all = T)
+
+## Creat another data frame that only accounts for Accidents
+
